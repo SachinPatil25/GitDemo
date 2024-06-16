d535d93fac72dafbc3998f5e9629966b2d21fa3c
Sachin <sachinnpatil25@gmail.com>
2024-06-16 23:08:50 +0530

    This is commit from asian timezone

---
+++ src/test/java/rahulshettyacademy/test/GitNewCode.java
@@ -0,0 +1,17 @@
+package rahulshettyacademy.test;
+
+import org.testng.annotations.Test;
+
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
+
+public class GitNewCode {
+	@Test
+	public void Newcode()
+	{
+System.out.println("I am new code");
+
+	}
+}
+
+
